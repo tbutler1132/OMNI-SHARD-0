9b8df079abeece7936d6ec152fe42ecef410da73
Tim Butler <timbutler@Tims-MacBook-Pro.local>
2025-04-21 19:13:05 -0400

    quick updated script and folder names

--- lib/ontology/schema/loadSchema.ts
+++ lib/ontology/schema/loadSchema.ts
@@ -4,7 +4,7 @@ import path from "path";
 export async function loadSchema<T>(entity: string): Promise<T> {
   const schemaPath = path.resolve(
     __dirname,
-    `../../../../omni_shard-0/schema/semantic/${entity}.json`
+    `../../../../omni_shard-0/schema/ontology/${entity}.json` //TODO: Reference path against ontology entity
   );
   const raw = await fs.readFile(schemaPath, "utf-8");
   return JSON.parse(raw);
